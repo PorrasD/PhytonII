9ade2490cd7d50388ba32e3a7b6906a8d89787df
PorrasD <dannyporras1296@gmail.com>
2023-07-06 21:05:19 -0600

    version actualizada

---
+++ Laboratorio 1/Trig.py
@@ -0,0 +1,13 @@
+import math
+
+class Trig:
+    pi = math.pi
+
+    def sin(self):
+        return math.sin(self.pi)
+
+    def cos(self):
+        return math.cos(self.pi)
+
+    def tan(self):
+        return math.tan(self.pi)
---
+++ Laboratorio 1/main.py
@@ -0,0 +1,43 @@
+import datetime
+import Trig
+
+""""
+def log_operation(operation, result):
+    with open('log.txt', 'a') as log_file:
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        log_entry = f'{timestamp} - Operation: {operation}, Result: {result}\n'
+        log_file.write(log_entry)
+"""
+
+def main():
+    trig = Trig()
+
+    while True:
+        print("Seleccione una opción:")
+        print("1. Seno")
+        print("2. Coseno")
+        print("3. Tangente")
+        print("4. Salir")
+
+        choice = input("Elija una opción: ")
+
+        if choice == "1":
+            sin = trig.sin()
+            print("El seno de pi es:", sin)
+           ## log_operation("Seno", sin)
+        elif choice == "2":
+            cos = trig.cos()
+            print ("El coseno de pi es:" ,cos)
+           ## log_operation("Coseno", cos)
+        elif choice == "3":
+            tan = trig.tan()
+            print("La tangente de pi es: ",tan)
+            ##log_operation("Tangente", tan)
+        elif choice == "4":
+            print("Hasta Pronto!")
+            break
+        else:
+            print("Opción inválida. Por favor, elija nuevamente.")
+
+if __name__ == "__main__":
+    main()
