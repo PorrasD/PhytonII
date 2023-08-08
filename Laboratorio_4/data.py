658d243b18e7442c38b6e8f15060ef144e180734
PorrasD <dannyporras1296@gmail.com>
2023-08-07 23:18:41 -0600

    Laboratorio_4

---
+++ Laboratorio_4/data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as mpl
+
+#Traer datos con pandas
+
+dataframe = pd.read_csv('ventas.csv')
+
+#revenue calculado con pandas(ventas - gastos)  creando una nueva columna con el nombre revenue.
+
+dataresult= dataframe.assign(Revenue= dataframe["Ventas"] - dataframe["Gastos"])
+print(dataresult)
+
+# Codigo de grafico con matplotlib que indica el tamaño, los nombres, labels, eje x y y.
+
+mpl.figure(figsize=(8,8))
+mpl.plot(dataframe["Mes"], dataframe["Ventas"], label="Ventas")
+mpl.plot(dataframe["Mes"], dataframe["Gastos"], label="Gastos")
+mpl.title("Phyton II \n Laboratorio 4 \n csv chart" )
+mpl.xlabel("Meses")
+mpl.ylabel("Monto")
+mpl.legend()
+mpl.show()
+
